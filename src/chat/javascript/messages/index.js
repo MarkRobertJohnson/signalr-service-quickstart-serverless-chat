7633a0942512aee56ba73d25ebc7418379521a70
Mark Johnson <MarkRobertJohnson@outlook.com>
2019-04-05 16:16:46 -0700

    updated

--- src/chat/javascript/messages/index.js
+++ src/chat/javascript/messages/index.js
@@ -2,8 +2,18 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 module.exports = async function (context, req) {
+  const message = req.body;
+  message.sender = req.headers && req.headers['x-ms-client-principal-name'] || '';
+
+  let recipientUserId = message.recipient;
+  if (message.recipient) {
+      recipientUserId = message.recipient;
+      message.isPrivate = true;
+  }
+
   return {
-    "target": "newMessage",
-    "arguments": [ req.body ]
+      'userId': recipientUserId,
+      'target': 'newMessage',
+      'arguments': [ message ]
   };
 };
